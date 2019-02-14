fc838466c13cbc655fe595469ecd850c546cec86
Theerasak Tubrit <jigkoh3@Theerasaks-MacBook-Pro.local>
2019-02-14 20:31:04 +0700

    response with  json

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.get('/',(req,res)=>{
     res.send('Server Ready...')
 })
 
+app.get('/api/test', (req,res)=>{
+    res.jsonp({
+        status : 200,
+        data : {name: 'jigkoh'}
+    });
+})
+
 app.listen(3000,()=>{
     console.log("server is start on port 3000");
 })
