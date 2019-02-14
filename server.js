31df8f5572e409aeead4db633b46601506c901d4
Theerasak Tubrit <jigkoh3@Theerasaks-MacBook-Pro.local>
2019-02-14 20:38:10 +0700

    used body-parser

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.send('Server Ready...')
@@ -10,6 +14,12 @@ app.get('/api/test', (req,res)=>{
         status : 200,
         data : {name: 'jigkoh'}
     });
+})
+
+app.post('/api/test', (req,res)=>{
+    //ไม่สามารถอ่าน request body ที่เป็น JSON ได้จึงไม่สามารถ respons json ได้
+    //วิธีแก้ไขคือใช้ body-parser middleware มาช่วย
+    res.jsonp(req.body);
 })
 
 app.listen(3000,()=>{
